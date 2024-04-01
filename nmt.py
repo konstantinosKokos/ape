3127e97e5a38354c87bb959a29e3cd4b70a972c8
konstantinosKokos <konstantinos@riseup.net>
2024-04-01 14:53:08 +0300

    fix seed and eval placement

--- nmt.py
+++ nmt.py
@@ -51,9 +51,9 @@ def run(
     start_time = time.time()
     ddp_setup(rank, world_size)
 
-    smoke_test = torch.tensor(rank)
+    smoke_test = torch.tensor(rank, device=rank)
     print(f'{smoke_test} @ {rank}')
-    smoke_test = dist.all_reduce(smoke_test, device=rank)
+    smoke_test = dist.all_reduce(smoke_test)
     print(f'{smoke_test} @ {rank}')
 
     train_set, dev_set = load_datasets(data_path, subsets=('train', 'dev'))
