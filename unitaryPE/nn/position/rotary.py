f851b9bf70672c07e19ad7f5ad87a74af334d9f4
konstantinosKokos <konstantinos@riseup.net>
2024-05-21 11:08:21 +0300

    no None-grad complaints

--- unitaryPE/nn/position/rotary.py
+++ unitaryPE/nn/position/rotary.py
@@ -1,33 +1,30 @@
-"""
-    Adapted from https://github.com/huggingface/transformers/blob/v4.35.2/src/transformers/models/roformer/modeling_roformer.p
-"""
-
 from __future__ import annotations
 
 
 import torch
 from torch import Tensor
-from torch.nn import Embedding, Parameter
+from torch.nn import Module
+from torch.nn.functional import embedding
 
 from .schemes import AtnFn, multihead_atn_fn
 
 
-class Rotary(Embedding):
+class Rotary(Module):
+    weight: Tensor
+
     def __init__(self, num_positions: int, embedding_dim: int) -> None:
-        super().__init__(num_positions, embedding_dim)
-        self.weight = Parameter(self._init_weight(self.weight.data))
+        super().__init__()
+        self.register_buffer('weight', self._init_weight(num_positions, embedding_dim))
 
     @staticmethod
-    def _init_weight(out: Tensor) -> Tensor:
-        n_pos, dim = out.shape
+    def _init_weight(n_pos: int, dim: int) -> Tensor:
+        out = torch.zeros(n_pos, dim, dtype=torch.float)
         position_enc = torch.tensor(
             [[pos / (10000 ** (2 * (j // 2) / dim)) for j in range(dim)] for pos in range(n_pos)]
         )
-        out.requires_grad = False
         sentinel = dim // 2 if dim % 2 == 0 else (dim // 2) + 1
         out[:, 0:sentinel] = torch.sin(position_enc[:, 0::2])
         out[:, sentinel:] = torch.cos(position_enc[:, 1::2])
-        out.detach_()
         return out
 
     @torch.no_grad()
@@ -35,7 +32,7 @@ class Rotary(Embedding):
         positions = torch.arange(
             start=0,  end=max_seq_len, dtype=torch.long, device=self.weight.device
         )
-        return super().forward(positions)
+        return embedding(positions, self.weight)
     
     def adjust_attention(self, sinusoidal_pos: Tensor) -> AtnFn:
         def f(
